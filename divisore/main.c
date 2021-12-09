08341f8684e41fcb7430514f24ba1fd861dd0377
darksnake <93006390+darksnakepy@users.noreply.github.com>
2021-12-09 12:31:09 +0100

    Update main.c

--- divisore/main.c
+++ divisore/main.c
@@ -7,43 +7,30 @@ int main()
 	int n, divisore, numDiv;
 	bool cont = true;
 
-	printf("Scrivi un numero maggiore di 0\n");
-	scanf("%d", &n);
-
-	if (n > 0)
-	{
-		divisore = n;
-		while (divisore > 0)
+	do
+	{	
+		printf("Scrivi un numero maggiore di 0\n");
+		scanf("%d", &n);
+		if(n>0)
 		{
-			numDiv = n % divisore;
-			if (numDiv == 0)
+			divisore = n;
+			while(divisore>0)
 			{
-				printf("%d\n", divisore);
-			}
-			divisore = divisore - 1;
-		}
-
-		while (cont == true)
-		{
-			printf("Scrivi un numero  di 0\n");
-			scanf("%d", &n);
-			if (n > 0)
-			{
-				divisore = n;
-				while (divisore > 0)
+				numDiv = n%divisore;
+				if(numDiv==0)
 				{
-					numDiv = n % divisore;
-					if (numDiv == 0)
-					{
-						printf("%d\n", divisore);
-					}
-					divisore = divisore - 1;
+					printf("%d\n", divisore);
 				}
+				divisore = divisore-1;
 			}
-			else
-			{
-				cont == false;
-			}
+		}
+		else
+		{
+			cont = false;
 		}
 	}
+		while(cont==true);
+		{
+			printf("Il numero e' uguale a 0!\n");
+		}
 }
