6839e59a5d2d4eec7aab3e3ebcc9ec220eb64127
darksnake <93006390+darksnakepy@users.noreply.github.com>
2022-05-02 09:19:55 +0200

    Update main.c

--- crociera/main.c
+++ crociera/main.c
@@ -8,35 +8,32 @@
 #define ncabine 10
 #define tipiCabine 4
 
-char path[] = { "file.txt" };
+char codiciCarte[100] =
+{
+    {"TUGQAJXMCV"},
+    {"JFYPNRVIQG"}
+};
 
 bool registered = false;
 bool cabinePiene = true;
 int nPersone = 0; // numero di persone registrate;
-int familyMembers = 0;
+int family = 0;
 char nomi[maxPersone];
 char cognomi[maxPersone];
 int day[maxPersone];
 int month[maxPersone];
 int year[maxPersone];
-int cabineOccupate4[ncabine]; // cabine per 4 
-int cabineOccupate3[ncabine];//cabine per 3
-int cabineOccupate2[ncabine];//cabine per 2
-int cabineOccupate1[ncabine]; //cabine singole
-void write();
-void read();
-void createFile();
+int cabineOccupate[4];
 
-float prezzoCabine[tipiCabine] = {99.90, 149.90, 199.90, 249.90};
+float prezzoCabine[tipiCabine] = { 99.90, 149.90, 199.90, 249.90 };
 
 
 
 int main()
 {
-    createFile();
     int choice;
     printf("Benvenuto in Costa Crociera\n[0] Registra utente e/o famiglia.\n[1] Utente gia registrato.\n");
-    scanf("%d", &choice);
+    scanf("%d>", &choice);
     do
     {
         switch (choice)
@@ -45,7 +42,6 @@ int main()
             registerUser();
             break;
         case 1:
-            assegnaCabine();
             break;
         }
     } while (true);
@@ -54,66 +50,113 @@ int main()
 int registerUser()
 {
     int choice, i;
+    bool cabinFull[4] = false;
+    bool isFamily = true;
     if (nPersone < 100)
     {
-        printf("Quante persone vogliono acquistare il biglietto\n");
-        scanf("%d", &familyMembers);
-        nPersone += familyMembers;
+        printf("Quante persone vogliono acquistare il biglietto?\n");
+        scanf("%d", &family);
+        nPersone += family;
         if (maxPersone - nPersone < 100)
         {
-          for (i = 0; i < familyMembers; i++)
+            for (i = 0; i < nPersone; i++)
+            {
+                printf("Persona numero [%d], perfavore inserire il nome\n", i);
+                scanf("%s> ", &nomi[i]);
+                printf("Persona numero [%d], perfavore inserire il cognome\n", i);
+                scanf("%s> ", &cognomi[i]);
+                printf("Persona numero [%d], inserisci la tua data di nascita (Giorno)\n", i);
+                scanf("%s> ", &day[i]);
+                if (day[i] > 31 && day[i] < 1)
                 {
-                    printf("Persona numero [%d], perfavore inserire il nome\n", i);
-                    scanf("%s> ", &nomi[i]);
-                    printf("Persona numero [%d], perfavore inserire il cognome\n", i);
-                    scanf("%s> ", &cognomi[i]);
-                    printf("Persona numero [%d], inserisci la tua data di nascita (Giorno)\n", i);
-                    scanf("%s> ", &day[i]);
-                    if (day[i] > 31 && day[i] < 1)
-                    {
-                        printf("Valore giorno inserito non corretto, reinserisci\n");
-                        scanf("%d> ", &day[i]);
-                    }
-                    printf("Persona numero [%d], inserisci la tua data di nascita (Mese)\n", i);
+                    printf("Valore giorno inserito non corretto, reinserisci\n");
+                    scanf("%d> ", &day[i]);
+                }
+                printf("Persona numero [%d], inserisci la tua data di nascita (Mese)\n", i);
+                scanf("%d> ", &month[i]);
+                while (month[i] > 12 && month[i] < 1)
+                {
+                    printf("Valore mese inserito non corretto, reinserisci");
                     scanf("%d> ", &month[i]);
-                    while (month[i] > 12 && month[i] < 1)
-                    {
-                        printf("Valore mese inserito non corretto, reinserisci");
-                        scanf("%d> ", &month[i]);
-                    }
-                    printf("Persona numero [%d], inserisci la tua data di nascita (Anno)\n", i);
-                    scanf("%d> ", &year[i]);
-                    while (year[i] > 2022 && month[i] < 1900)
-                    {
-                        printf("Valore anno inserito non corretto, reinserisci\n");
-                        scanf("%d> ", &month[i]);
-
-                    }
-                    write();
+                }
+                printf("Persona numero [%d], inserisci la tua data di nascita (Anno)\n", i);
+                scanf("%d> ", &year[i]);
+                while (year[i] > 2022 && month[i] < 1900)
+                {
+                    printf("Valore anno inserito non corretto, reinserisci\n");
+                    scanf("%d> ", &month[i]);
                 }
             }
         }
     }
-    
-
-
-int utenteRegistrato()
-{
-    printf("Se sei un utente gia registrato, inserisci il nome e cognome per verificare la validità del tuo biglietto\n");
-}
-
-
-
-int assegnaCabine()
-{
-    for (int i = 0; i < 4; i++)
+    switch (family)
     {
-        printf("%.2f\n%.2f\n%.2f\n%.2f", prezzoCabine[i]);
-
+    case 1:
+        if (cabineOccupate[0] <= 10)
+        {
+            cabineOccupate[0]++;
+            break;
+        }
+        {
+            cabinFull[0] = true;
+            printf("Non ci sono abbastanza posti\n");
+        }
+    case 2:
+        if (cabineOccupate[1] <= 10)
+        {
+            cabineOccupate[1]++;
+            break;
+        }
+        else
+        {
+            cabinFull[1] = true;
+            printf("Non ci sono abbastanza posti\n");
+        }
+    case 3:
+        if (cabineOccupate[2] <= 10)
+        {
+            cabineOccupate[2]++;
+            break;
+        }
+        else
+        {
+            cabinFull[2] = true;
+            printf("Non ci sono abbastanza posti\n");
+        }
+    case 4:
+        if (cabineOccupate[3] <= 10)
+        {
+            cabineOccupate[3]++;
+            break;
+        }
+        else
+        {
+            cabinFull[3] = true;
+            printf("Non ci sono abbastanza posti\n");
+        }
     }
-
 }
 
+void verificaTessera()
+{
+    char cardInput[] = { "" };
+    bool cardOn = true;
+    printf("Inserisci il numero della tua tessera\n");
+    scanf("%s", cardInput);
+    if (strcmp(codiciCarte, cardInput) == 0)
+    {
+        printf("Codice della carta valido, puoi continuare con gli acquisti\n");
+    }
+    else
+    {
+        printf("Codice della carta non valido\n");
+    }
+}
+
+void prezzoFinale()
+{
+    
+}
 
 
 int clear()
@@ -121,53 +164,3 @@ int clear()
     system("cls");
 }
 
-void write()
-{
-    FILE* file;
-    file = fopen(path, "w");
-    if (file == NULL)
-    {
-        printf("Errore, nessun database trovato\n");
-        exit(1);
-    }
-    for (int i = 0; i < nomi; i++)
-    {
-        fprintf(file, "%s ", nomi[i]);
-    }
-    for (int i = 0; i < cognomi; i++)
-    {
-        fprintf(file, "%s\n", cognomi[i]);
-    }
-    fclose(file);
-}
-void read()
-{
-    FILE* fp;
-    fp = fopen(path, "r");
-    if (fp == NULL)
-    {
-        printf("Errore, nessun database trovato\n");
-        exit(1);
-    }
-    for (int i = 0; i < nomi; i++)
-    {
-        fscanf(fp, "%s", &nomi[i]);
-    }
-    for (int i = 0; i < cognomi; i++)
-    {
-        fscanf(fp, "%s", &cognomi[i]);
-    }
-
-    fclose(fp);
-}
-void createFile()
-{
-    FILE* fp;
-    fp = fopen(path, "a");
-    if (fp == NULL)
-    {
-        printf("Errore, nessun database trovato\n");
-        exit(1);
-    }
-    fclose(fp);
-}
