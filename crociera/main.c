a014f465b3831c7e9c76893692e0318ee866bf8a
darksnake <93006390+darksnakepy@users.noreply.github.com>
2022-05-09 18:54:46 +0200

    Update main.c

--- crociera/main.c
+++ crociera/main.c
@@ -4,13 +4,16 @@
 #include <string.h>
 #include <time.h>
 
+// dichiaro funzioni 
+void clear();
+void database();
+
+// variabili costanti
 #define maxPersone 100
 #define ncabine 10
 #define tipiCabine 4
 
-char codiciCarte[100];
-
-int totFamiglie;
+int totFamiglie = 0;
 bool registered = false;
 bool cabinePiene = false;
 int nPersone = 0; // numero di persone registrate
@@ -20,14 +23,17 @@ char cognomi[maxPersone];
 int day[maxPersone];
 int month[maxPersone];
 int year[maxPersone];
+int lungFamiglia[maxPersone];
+int familyCont = 0;
 int cabineOccupate[4];
-void outPersone();
+void database();
+float prezzoFinale[maxPersone];
 
 
 int main()
 {
     int choice;
-    printf("Benvenuto in Costa Crociera!\n[0] Registra utente e/o famiglia\n[1] Bar\n[2] Gestione staff\n>");
+    printf("\n Benvenuto nel programma di registrazione di Costa Crociera!\n [0] Registra utente e/o famiglia\n [1] Gestione staff\n\n >");
     scanf("%d", &choice);
     do
     {
@@ -39,7 +45,7 @@ int main()
         case 1:
             break;
         case 2:
-            outPersone();
+            database();
             break;
 
         default:
@@ -52,6 +58,7 @@ int main()
 
 int registerUser()
 {
+    clear();
     int choice, i;
     bool cabinFull[4];
     bool isFamily = true;
@@ -59,109 +66,140 @@ int registerUser()
     {
         if (cabinePiene == false)
         {
-            printf("[0] Custom\n[1] Registra una famiglia\n");
+            printf("[0] Registra famiglia o custom\n[1] Viaggia\n[3] Torna indietro\n");
         }
         if (nPersone < 100)
         {
-            scanf("%d", &choice);
-
+            scanf("%d", &choice);  
+            clear();
             if (choice == 0 && nPersone < 100)
             {
-                do
+                printf("Inserisci il numero di utenti che vuoi registrare\n");
+                scanf("%d", &family);
+                nPersone += family;
+                if (maxPersone - nPersone < 100)
                 {
-                    printf("Inserisci il numero di utenti che vuoi registrare\n");
-                    scanf("%d", &family);
-                    nPersone += family;
-                    if (maxPersone - nPersone < 100)
+                    for (i = totFamiglie ; i < family + totFamiglie; i++)
                     {
-                        for (i = totFamiglie ; i < nPersone - totFamiglie; i++)
+                        printf("Persona numero [%d], perfavore inserire il nome\n", i);
+                        scanf("%s> ", &nomi[i]);
+                        printf("Persona numero [%d], perfavore inserire il cognome\n", i);
+                        scanf("%s> ", &cognomi[i]);
+                        printf("Persona numero [%d], inserisci la tua data di nascita (Giorno)\n", i);
+                        scanf("%s> ", &day[i]);
+                        if (day[i] < 1 || day[i] > 31)
                         {
-                            printf("Persona numero [%d], perfavore inserire il nome\n", i);
-                            scanf("%s> ", &nomi[i]);
-                            printf("Persona numero [%d], perfavore inserire il cognome\n", i);
-                            scanf("%s> ", &cognomi[i]);
-                            printf("Persona numero [%d], inserisci la tua data di nascita (Giorno)\n", i);
-                            scanf("%s> ", &day[i]);
-                            if (day[i] < 1 || day[i] > 31)
-                            {
-                                printf("Valore giorno inserito non corretto, reinserisci\n");
-                                scanf("%d> ", &day[i]);
-                            }
-                            printf("Persona numero [%d], inserisci la tua data di nascita (Mese)\n", i);
+                            printf("Valore giorno inserito non corretto, reinserisci\n");
+                            scanf("%d> ", &day[i]);
+                        }
+                        printf("Persona numero [%d], inserisci la tua data di nascita (Mese)\n", i);
+                        scanf("%d> ", &month[i]);
+                        while (month[i] < 1 || month[i] > 12)
+                        {
+                            printf("Valore mese inserito non corretto, reinserisci");
                             scanf("%d> ", &month[i]);
-                            while (month[i] < 1 || month[i] > 12)
-                            {
-                                printf("Valore mese inserito non corretto, reinserisci");
-                                scanf("%d> ", &month[i]);
-                            }
-                            printf("Persona numero [%d], inserisci la tua data di nascita (Anno)\n", i);
-                            scanf("%d> ", &year[i]);
-                            while (year[i] < 1900 || year[i] > 2022)
-                            {
-                                printf("Valore anno inserito non corretto, reinserisci\n");
-                                scanf("%d> ", &month[i]);
-                            }
                         }
-                        totFamiglie += family;
-                        switch (family)
+                        printf("Persona numero [%d], inserisci la tua data di nascita (Anno)\n", i);
+                        scanf("%d> ", &year[i]);
+                        while (year[i] < 1900 || year[i] > 2022)
                         {
-                        case 1:
-                            if (cabineOccupate[0] <= 10)
-                            {
-                                cabineOccupate[0]++;
-                                break;
-                            }
-                            else
-                            {
-                                cabinFull[0] = true;
-                                printf("Non ci sono abbastanza posti\n");
-                                break;
-                            }
-                        case 2:
-                            if (cabineOccupate[1] <= 10)
-                            {
-                                cabineOccupate[1]++;
-                                break;
-                            }
-                            else
-                            {
-                                cabinFull[1] = true;
-                                printf("Non ci sono abbastanza posti\n");
-                            }
-                        case 3:
-                            if (cabineOccupate[2] <= 10)
-                            {
-                                cabineOccupate[2]++;
-                                break;
-                            }
-                            else
-                            {
-                                cabinFull[2] = true;
-                                printf("Non ci sono abbastanza posti\n");
-                            }
-                        case 4:
-                            if (cabineOccupate[3] <= 10)
-                            {
-                                cabineOccupate[3]++;
-                                break;
-                            }
-                            else
-                            {
-                                cabinFull[3] = true;
-                                printf("Non ci sono abbastanza posti\n");
-                            }
-                        default:
-                            printf("Errore");
-                            break;
-                        }
-
-                        if (cabinFull[0] && cabinFull[1] && cabinFull[2] && cabinFull[3])
-                        {
-                            cabinePiene = true;
+                            printf("Valore anno inserito non corretto, reinserisci\n");
+                            scanf("%d> ", &month[i]);
                         }
                     }
 
-                } while (true);
+                    switch (family)
+                    {
+                    case 1:
+                        if (cabineOccupate[0] <= 10)
+                        {
+                            cabineOccupate[0]++;
+                            lungFamiglia[familyCont] = family;
+                            familyCont++;
+                            nPersone += family;
+                            totFamiglie += family;
+                            break;
+                        }
+                        else
+                        {
+                            cabinFull[0] = true;
+                            printf("Non ci sono abbastanza posti\n");
+                        }
+                        break;
+                    case 2:
+                        if (cabineOccupate[1] <= 10)
+                        {
+                            cabineOccupate[1]++;
+                            lungFamiglia[familyCont] = family;
+                            familyCont++;
+                            nPersone += family;
+                            totFamiglie += family;
+                            break;
+                        }
+                        else
+                        {
+                            cabinFull[1] = true;
+                            printf("Non ci sono abbastanza posti\n");
+                        }
+                        break;
+                    case 3:
+                        if (cabineOccupate[2] <= 10)
+                        {
+                            cabineOccupate[2]++;
+                            lungFamiglia[familyCont] = family;
+                            familyCont++;
+                            nPersone += family;
+                            totFamiglie += family;
+                            break;
+                        }
+                        else
+                        {
+                            cabinFull[2] = true;
+                            printf("Non ci sono abbastanza posti\n");
+                        }
+                        break;
+                    case 4:
+                        if (cabineOccupate[3] <= 10)
+                        {
+                            cabineOccupate[3]++;
+                            lungFamiglia[familyCont] = family;
+                            familyCont++;
+                            nPersone += family;
+                            totFamiglie += family;
+                            break;
+                        }
+                        else
+                        {
+                            cabinFull[3] = true;
+                            printf("Non ci sono abbastanza posti\n");
+                        }
+                        break;
+                    default:
+                        printf("Errore");
+                        break;
+                    }
+
+                    if (cabinFull[0] && cabinFull[1] && cabinFull[2] && cabinFull[3])
+                    {
+                        cabinePiene = true;
+                    }
+                }
+            }
+            else if(choice == 1)
+            {
+                printf("Perfetto, Buon viaggio!");
+                Sleep(1000);
+                main();
+            }
+            else if(choice==3)
+            {
+                main();
+            }
+            else
+            {
+                printf("Scelta sbagliata!\n");
+                Sleep(5000);
+                registerUser();
             }
         }
     
@@ -169,7 +207,7 @@ int registerUser()
 }
 
 
-void outPersone()
+void database()
 {
     for (int i = 0; i < 4; i++)
     {
@@ -177,7 +215,7 @@ void outPersone()
     }
     if (cabinePiene = true)
     {
-        printf("Le cabine sono piene!\n");
+        printf("La nave e' al completo!\n");
     }
     for (int i = 0; i < nPersone; i++)
     {
